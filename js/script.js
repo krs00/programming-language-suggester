ead229c1bbc918607ebfe20761436ec5df65c3db
Kymani Stephens <kymanist00@gmail.com>
2023-03-26 15:14:32 -0700

    configure each question to grab language value

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 // Get DOM nodes
-// Selects entire form
-const form = document.getElementById("lang-form")
+// Selects ID for each form section
+const colorForm = document.getElementById("question-color")
+const sandwichForm = document.getElementById("question-sandwich")
+const movieForm = document.getElementById("question-movie")
+const dessertForm = document.getElementById("question-dessert")
+const bookForm = document.getElementById("question-book") 
 //Selects <p> tag for display results
 const display = document.getElementById("display")
 
 
 // Function will determine value for each answer 
 function handleSelect(event) {
+    // STOPS PAGE FROM REFRESHING
     event.preventDefault();
 
-    const questionColor = form.querySelector('input[name=color]:checked').value;
-    console.log(questionColor) 
+    // GETS VALUE OF COLOR QUESTION 
+    const questionColor = colorForm.querySelector('input[name=color]:checked').value;
+    console.log(questionColor)
+
+     // GETS VALUE OF SANDWICH QUESTION 
+     const questionSandwich = sandwichForm.querySelector('input[name=sandwich]:checked').value;
+     console.log(questionSandwich)
+
+      // GETS VALUE OF MOVIE QUESTION 
+      const questionMovie = movieForm.querySelector('input[name=movie]:checked').value;
+      console.log(questionMovie) 
+
+       // GETS VALUE OF DESSERT QUESTION 
+     const questionDessert = dessertForm.querySelector('input[name=dessert]:checked').value;
+     console.log(questionDessert)
+
+      // GETS VALUE OF BOOK QUESTION 
+      const questionBook = bookForm.querySelector('input[name=book]:checked').value;
+      console.log(questionBook)
+
+
+
 };
 
 
