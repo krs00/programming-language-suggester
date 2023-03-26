33ec82ba4bfda16bbba301644a676477a2214a3a
Kymani Stephens <kymanist00@gmail.com>
2023-03-26 13:03:22 -0700

    add function to grab values of checked radio btns

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
+// Get DOM nodes
+// Selects entire form
+const form = document.getElementById("lang-form")
+//Selects <p> tag for display results
+const display = document.getElementById("display")
+
+
+// Function will determine value for each answer 
+function handleSelect(event) {
+    event.preventDefault();
+
+    const questionColor = form.querySelector('input[name=color]:checked').value;
+    console.log(questionColor) 
+};
 
 
 
 
 
-
-
-// adds event listener on submit of form,
+// adds event listener on submit of form, triggers handleSelect function
 document.getElementById("lang-form").addEventListener("submit", handleSelect)
 
