7615d58f3b1695a896efc1b930556777267ac8a5
Kymani Stephens <kymanist00@gmail.com>
2023-03-26 22:14:58 -0700

    add variable for first name

--- js/script.js
+++ js/script.js
@@ -4,13 +4,11 @@ const colorForm = document.getElementById("question-color")
 const sandwichForm = document.getElementById("question-sandwich")
 const movieForm = document.getElementById("question-movie")
 const dessertForm = document.getElementById("question-dessert")
-const bookForm = document.getElementById("question-book") 
-//Selects <p> tag for display results
-const display = document.getElementById("display")
-
+const bookForm = document.getElementById("question-book")
 
 // Function will determine value for each answer 
-function handleSelect(event) { 
+function handleSelect(event) {
+
     // STOPS PAGE FROM REFRESHING
     event.preventDefault();
 
@@ -35,15 +33,74 @@ function handleSelect(event) {
     let pythonScore = 0;
     let cSharpScore = 0;
 
-    // INCREMENT 
-
+    // INCREMENT SCORES FOR EACH QUESTION
+    // QUESTION #1 COLOR
     if (questionColor === "JAVA") {
         javaScore++;
+    } else if (questionColor === "PYTHON") {
+        pythonScore++;
+    } else if (questionColor === "C#") {
+        cSharpScore++;
+    };
+
+    // QUESTION #1 COLOR
+    if (questionColor === "JAVA") {
+        javaScore++;
+    } else if (questionColor === "PYTHON") {
+        pythonScore++;
+    } else if (questionColor === "C#") {
+        cSharpScore++;
+    };
+
+    // QUESTION #2 SANDWICH
+    if (questionSandwich === "JAVA") {
+        javaScore++;
+    } else if (questionSandwich === "PYTHON") {
+        pythonScore++;
+    } else if (questionSandwich === "C#") {
+        cSharpScore++;
+    };
+
+       // QUESTION #3 MOVIE
+       if (questionMovie === "JAVA") {
+        javaScore++;
+    } else if (questionMovie === "PYTHON") {
+        pythonScore++;
+    } else if (questionMovie === "C#") {
+        cSharpScore++;
+    };
+
+       // QUESTION #4 DESSERT
+       if (questionDessert === "JAVA") {
+        javaScore++;
+    } else if (questionDessert === "PYTHON") {
+        pythonScore++;
+    } else if (questionDessert === "C#") {
+        cSharpScore++;
+    };
+
+       // QUESTION #5 BOOK
+       if (questionBook === "JAVA") {
+        javaScore++;
+    } else if (questionBook === "PYTHON") {
+        pythonScore++;
+    } else if (questionBook === "C#") {
+        cSharpScore++;
+    };
+
+    // Selects Display Node
+    const displayTxt = document.getElementById("display")
+    // GET FIRST NAME AND CAPITALIZE FIRST LETTER
+    const firstName = document.getElementById("fname").value.toLowerCase()
+    const Name = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+
+    // OUTPUT RESULTS TO SCREEN 
+    if (javaScore > pythonScore && javaScore > cSharpScore) {
+        displayTxt.innerText = `${Name}, you should learn Java!`
     }
 
-
-
 };
+
 
 
 
